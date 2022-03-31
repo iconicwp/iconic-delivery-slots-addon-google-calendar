b1906aec60670a5518612a7984b012b02d70e73c
Pramod Jodhani <mrpramodjodhani@gmail.com>
2022-03-31 16:38:57 +0530

    change text domain in strings

--- inc/admin/settings.php
+++ inc/admin/settings.php
@@ -25,33 +25,33 @@ function jckwds_gcal_settings( $wpsf_settings ) {
 	if ( ! jckwds_gcal_search_tab( $wpsf_settings['tabs'], 'integrations' ) ) {
 		$wpsf_settings['tabs'][] = array(
 			'id'    => 'integrations',
-			'title' => __( 'Integrations', 'jckwds' ),
+			'title' => __( 'Integrations', 'iconic-wds-gcal' ),
 		);
 	}
 
 	$gcal_fields = array(
 		array(
 			'id'       => 'api',
-			'title'    => __( 'API Key', 'jckwds' ),
+			'title'    => __( 'Client ID', 'iconic-wds-gcal' ),
 			'subtitle' => '',
 			'type'     => 'text',
 		),
 		array(
 			'id'       => 'secret',
-			'title'    => __( 'Secret', 'jckwds' ),
+			'title'    => __( 'Client Secret', 'iconic-wds-gcal' ),
 			'subtitle' => '',
 			'type'     => 'text',
 		),
 		array(
 			'id'       => 'redirect_url',
-			'title'    => __( 'Redirect URL', 'jckwds' ),
+			'title'    => __( 'Redirect URL', 'iconic-wds-gcal' ),
 			'subtitle' => '',
 			'type'     => 'custom',
 			'output'   => array( 'Iconic_WDS_Gcal_Google_Calendar', 'generate_redirect_url_field' ),
 		),
 		array(
 			'id'       => 'authenication_button',
-			'title'    => __( 'Authenticate', 'jckwds' ),
+			'title'    => __( 'Authenticate', 'iconic-wds-gcal' ),
 			'subtitle' => '',
 			'type'     => 'custom',
 			'output'   => array( 'Iconic_WDS_Gcal_Google_Calendar', 'get_auth_button' ),
@@ -61,7 +61,7 @@ function jckwds_gcal_settings( $wpsf_settings ) {
 	if ( Iconic_WDS_Gcal_Google_Calendar::is_connection_active() ) {
 		$gcal_fields[] = array(
 			'id'       => 'select_calendar',
-			'title'    => __( 'Calendar', 'jckwds' ),
+			'title'    => __( 'Calendar', 'iconic-wds-gcal' ),
 			'subtitle' => '',
 			'type'     => 'select',
 			'choices'  => Iconic_WDS_Gcal_Google_Calendar::get_calendars(),
@@ -69,31 +69,30 @@ function jckwds_gcal_settings( $wpsf_settings ) {
 	}
 
 	$wpsf_settings['sections'][] = array(
-		'tab_id'              => 'integrations',
-		'section_id'          => 'google',
-		'section_title'       => __( 'Google Calender Authentication', 'jckwds' ),
-		'section_description' => __( '', 'jckwds' ),
-		'section_order'       => 0,
-		'fields'              => $gcal_fields,
+		'tab_id'        => 'integrations',
+		'section_id'    => 'google',
+		'section_title' => __( 'Google Calender Authentication', 'iconic-wds-gcal' ),
+		'section_order' => 0,
+		'fields'        => $gcal_fields,
 	);
 
 	$wpsf_settings['sections'][] = array(
 		'tab_id'              => 'integrations',
 		'section_id'          => 'google_setting',
-		'section_title'       => __( 'Google Calender Settings', 'jckwds' ),
+		'section_title'       => __( 'Google Calender Settings', 'iconic-wds-gcal' ),
 		'section_description' => __( 'Available placeholders: {SITE_NAME}, {ORDER_NUMBER}, {ORDER_STATUS}, {ORDER_DATE_TIME}, {DELIVERY_DATE_TIME}, {CUSTOMER_NAME}, {CUSTOMER_EMAIL}, {CUSTOMER_ADDRESS}, {CUSTOMER_PHONE}, {NOTE} and {CART_ITEMS}.', 'jckwds' ),
 		'section_order'       => 1,
 		'fields'              => array(
 			array(
 				'id'       => 'event_title',
-				'title'    => __( 'Event Title', 'jckwds' ),
+				'title'    => __( 'Event Title', 'iconic-wds-gcal' ),
 				'subtitle' => '',
 				'default'  => '{SITE_NAME} - Order #{ORDER_NUMBER}',
 				'type'     => 'text',
 			),
 			array(
 				'id'       => 'event_description',
-				'title'    => __( 'Event Description', 'jckwds' ),
+				'title'    => __( 'Event Description', 'iconic-wds-gcal' ),
 				'subtitle' => '',
 				'default'  => "Order #{ORDER_NUMBER} on {SITE_NAME}.\n\n{CART_ITEMS} ",
 				'type'     => 'textarea',
